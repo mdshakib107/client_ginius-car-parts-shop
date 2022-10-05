38f4a0c0d0719688096e318f5f72eb8710429df3
mdshakib107 <mdshakib105100@gmail.com>
2022-10-05 22:25:05 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 >This is Card parts SHop Site ok!!!!</h1>
+      <h2>I will working on this Project sooon !!!!</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcnkk2n-Qz7gG2JyQl4IDevpgdI0QU0PM",
+  authDomain: "ginius-car-surveses.firebaseapp.com",
+  projectId: "ginius-car-surveses",
+  storageBucket: "ginius-car-surveses.appspot.com",
+  messagingSenderId: "774979544657",
+  appId: "1:774979544657:web:c20bb7603dd25366c3fa70"
+};
+
+// Initialize Firebase
+  
+
+const app = initializeApp(firebaseConfig);
+
+   
